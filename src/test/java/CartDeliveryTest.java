185b87c5d8b25a9a0b4891ccc7baeb6914c2e6c3
Moks <gutamax2@mail.ru>
2022-02-18 19:41:29 +0500

    fix test and add jar

--- src/test/java/CartDeliveryTest.java
+++ src/test/java/CartDeliveryTest.java
@@ -35,26 +35,26 @@ public class CartDeliveryTest {
                 .generateInfo("ru");
         String planningDate = generateDate(3);
         open("http://localhost:9999/");
-//        $("[data-test-id='city'] input").val(info.getCity());
-//        $("[type='tel']").sendKeys(Keys.CONTROL + "a");
-//        $("[type='tel']").sendKeys(Keys.BACK_SPACE);
-//        $("[type='tel']").val(generateDate(3));
-//        $("[data-test-id='name'] input").val(info.getName());
-//        $("[name='phone']").val(info.getPhoneNumber());
-//        $("[class='checkbox__box']").click();
-//        $("[class='button__text']").click();
-//        SelenideElement blocks = $("[data-test-id='success-notification']");
-//        blocks.$("[class= 'notification__content']").shouldBe(visible, Duration.ofSeconds(15))
-//                .shouldHave(exactText("Встреча успешно запланирована на " + planningDate));
-//        $("[type='tel']").sendKeys(Keys.CONTROL + "a");
-//        $("[type='tel']").sendKeys(Keys.BACK_SPACE);
-//        $("[type='tel']").val(generateDate(5));
-//        $("[class='button__text']").click();
-//        SelenideElement block = $("[data-test-id='replan-notification']");
-//        block.$("[class= 'notification__content']").shouldBe(visible);
-//        $("[class='button button_view_extra button_size_s button_theme_alfa-on-white']").click();
-//        $("[class= 'notification__content']").shouldBe(visible, Duration.ofSeconds(15))
-//                .shouldHave(exactText("Встреча успешно запланирована на " + generateDate(5)));
+        $("[data-test-id='city'] input").val(info.getCity());
+        $("[type='tel']").sendKeys(Keys.CONTROL + "a");
+        $("[type='tel']").sendKeys(Keys.BACK_SPACE);
+        $("[type='tel']").val(generateDate(3));
+        $("[data-test-id='name'] input").val(info.getName());
+        $("[name='phone']").val(info.getPhoneNumber());
+        $("[class='checkbox__box']").click();
+        $("[class='button__text']").click();
+        SelenideElement blocks = $("[data-test-id='success-notification']");
+        blocks.$("[class= 'notification__content']").shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно запланирована на " + planningDate));
+        $("[type='tel']").sendKeys(Keys.CONTROL + "a");
+        $("[type='tel']").sendKeys(Keys.BACK_SPACE);
+        $("[type='tel']").val(generateDate(5));
+        $("[class='button__text']").click();
+        SelenideElement block = $("[data-test-id='replan-notification']");
+        block.$("[class= 'notification__content']").shouldBe(visible);
+        $("[class='button button_view_extra button_size_s button_theme_alfa-on-white']").click();
+        $("[class= 'notification__content']").shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно запланирована на " + generateDate(5)));
 
 
     }
